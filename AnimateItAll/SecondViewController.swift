eb12d3d07e16b8a601d07f4c0ad30af1ac20dab6
Justin Stanley <justin.stanley@skipthedishes.ca>
2017-11-11 08:52:11 -0600

    Initial Commit

---
+++ AnimateItAll/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AnimateItAll
+//
+//  Created by Justin Stanley on 2017-11-11.
+//  Copyright © 2017 Justin Stanley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
